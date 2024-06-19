eb0c4e983abe3a144c34a55932016ad254831e47
keremmwx666 <kerempak4@gmail.com>
2024-06-19 19:04:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 573996298269884435
+CHANNEL_ID = 609710169974374401
 SELF_MUTE = True
 SELF_DEAF = False
 
